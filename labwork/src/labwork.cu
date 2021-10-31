1bc155fc5ab4be35ecd98d352d22e4a373ab79f7
hieuht <goldenrezgun@gmail.com>
2021-10-31 04:09:20 -0400

    UPDATE: labwork 1 code

--- labwork/src/labwork.cu
+++ labwork/src/labwork.cu
@@ -110,6 +110,7 @@ void Labwork::labwork1_OpenMP() {
     int pixelCount = inputImage->width * inputImage->height;
     outputImage = static_cast<char *>(malloc(pixelCount * 3));
     // do nothing here
+    // omp_set_num_threads(2);
     #pragma omp parallel for
     for (int j = 0; j < 100; j++) {     // let's do it 100 times, otherwise it's too fast!
         
@@ -180,7 +181,10 @@ void Labwork::labwork3_GPU() {
 void Labwork::labwork4_GPU() {
 }
 
-void Labwork::labwork5_GPU(bool shared) {
+void Labwork::labwork5_CPU() {
+}
+
+void Labwork::labwork5_GPU() {
 }
 
 void Labwork::labwork6_GPU() {
